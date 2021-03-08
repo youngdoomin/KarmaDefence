55a738156e988a4595a3f320d9fcccd873591f3c
minyoungdoo <dudenx01@naver.com>
2021-03-09 00:41:23 +0900

    버그 수정
    - 폭발 투사체 스크립트에 오류 메세지 뜨는 현상 수정
    - 씬과 팝업 전환시 시간 설정 버그 수정
    - 모바일 구동시 가로로 되게 함

--- Assets/Script/Common/Damage/ProjectileExp.cs
+++ Assets/Script/Common/Damage/ProjectileExp.cs
@@ -4,68 +4,24 @@ using UnityEngine;
 
 public class ProjectileExp : ProjectileDefault
 {
-    // public float ExpRange;  // 광역 데미지 범위
-    // private Vector3 boxSize;
-    // BoxCollider box;
     List<GameObject> currentCollisions = new List<GameObject>();
-    // Start is called before the first frame update
-    protected override void Start()
-    {
-        // box = transform.GetChild(0).GetComponent<BoxCollider>();
-        // boxSize = new Vector3(ExpRange, ExpRange, ExpRange);
 
-    }
-    /*
-    private void OnDisable()
-    {
-        box.enabled = true;
-    }
-    */
     protected override void OnTriggerEnter(Collider coll)
     {
         if (coll.gameObject.tag == this.gameObject.tag)
         {
             currentCollisions.Add(coll.gameObject);
-            // Collider[] explo= Physics.OverlapBox(transform.position, boxSize, transform.rotation);
             
             transform.GetChild(transform.childCount - 1).gameObject.SetActive(true);
             transform.GetChild(transform.childCount - 2).gameObject.SetActive(false);
             GetComponent<Rigidbody>().velocity = Vector3.zero;
 
-            foreach (GameObject gObject in currentCollisions)
+            for (int i = 0; i < currentCollisions.Count; i++)
             {
-                gObject.GetComponent<UnitHp>().Damaged(int.Parse(this.gameObject.name), "Magic");
-                currentCollisions.Remove(gObject);
-                // coll.SendMessage("Damaged", tempStorage);
+                currentCollisions[i].GetComponent<UnitHp>().Damaged(int.Parse(this.gameObject.name), "Magic");
+                currentCollisions.Remove(currentCollisions[i]);
                 Invoke("False", 1);
             }
-
-            /*
-            object[] tempStorage = new object[2];
-            tempStorage[0] = int.Parse(this.gameObject.name);
-            tempStorage[1] = "Magic";
-            */
-            /*
-            foreach (var col in explo)
-            {
-                if (col.gameObject.tag == this.gameObject.tag)
-                {
-                    col.GetComponent<UnitHp>().Damaged(int.Parse(this.gameObject.name), "Magic");
-                    // coll.SendMessage("Damaged", tempStorage);
-                    Invoke("False", 1);
-                }
-            }
-            */
-            // box.enabled = false;
         }
     }
-
-    /*
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        // Gizmos.DrawSphere(transform.position, ExpRange);
-        Gizmos.DrawCube(transform.position, new Vector3(ExpRange, ExpRange, ExpRange));
-    }
-    */
 }
